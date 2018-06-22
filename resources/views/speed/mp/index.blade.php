397927de21fa0755adea25d262565280434a3d01
wang zaron <wangzaron@wangs-iMac.local>
2018-06-22 19:43:41 +0800

    fix

---
+++ resources/views/speed/mp/index.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>37学车</title>
+        <meta name="keywords" content="学车,考驾照,驾校,报名">
+        <meta http-equiv="charset" content="utf-8">
+        <meta http-equiv="content-type" content="text/html">
+        <meta http-equiv="Accept-Language" content="zh_CN">
+        {{--<meta http-equiv="refresh" content="0">--}}
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+    </head>
+    <body>
+
+    </body>
+    <script src="{{asset('js/app.js')}}"></script>
+</html>
